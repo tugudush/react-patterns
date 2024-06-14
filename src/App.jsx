5a7263524a6cef6b342367470fd1345ecf91c14e
tugudush <jerome2kph@gmail.com>
2024-06-15 06:51:29 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+  return (
+    <main>
+      <section>
+        <h2>Why work with us?</h2>
+        <Accordion className="accordion">
+
+        </Accordion>
+      </section>
+    </main>
+  )
 }
 
 export default App;
