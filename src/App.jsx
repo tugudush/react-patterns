c85f878829a9763234cd0ce52f09d9a30d9b9fb9
tugudush <jerome2kph@gmail.com>
2024-06-15 06:57:57 +0800

    progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Accordion from "./components/Accordion/Accordion";
+import AccordionItem from "./components/Accordion/AccordionItem";
 
 function App() {
   return (
@@ -6,11 +7,25 @@ function App() {
       <section>
         <h2>Why work with us?</h2>
         <Accordion className="accordion">
+          <AccordionItem title="We got 20 years of experience">
+            <article>
+              <p>You can't go wrong with us.</p>
+              <p>We are in the business of p</p>
+            </article>
+          </AccordionItem>
 
+          <AccordionItem title="We're working with the local guides">
+            <article>
+              <p>Exercitation amet et quis enim aute amet amet eu sint.</p>
+              <p>
+                Officia anim dolore cupidatat elit elit id id duis laboris nisi.
+              </p>
+            </article>
+          </AccordionItem>
         </Accordion>
       </section>
     </main>
-  )
+  );
 }
 
 export default App;
